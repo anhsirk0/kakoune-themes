f085e408a060686041bec1a0cd6a3afab05a5859
krishna <krishna404@yandex.com>
2022-09-09 19:22:03 +0530

    added change-theme

---
+++ change-theme.pl
@@ -0,0 +1,42 @@
+#! /usr/bin/env perl
+
+my $dir = $ENV{HOME} . "/.config/kak";
+my $config = $ENV{HOME} . "/.config/kak/kakrc";
+my $config_content;
+my $theme;
+
+my @all_themes = split "\n", `ls $dir/colors`;
+my @default_themes = split "\n", `ls /usr/share/kak/colors`;
+push @all_themes, @default_themes;
+
+if (scalar @ARGV > 0) {
+    ($theme) = grep /$ARGV[0]/, @all_themes;
+    if ($theme) {
+	print "\'$theme\' theme selected\n";
+    } else {
+	print "No theme found for \'$ARGV[0]\'\n"; 
+    }
+}
+
+unless($theme) {
+    my $joined_themes = join "\n", @all_themes;
+    $theme = `echo "$joined_themes" | fzf`;
+    chomp $theme;
+}
+
+if ($theme) {
+    $theme =~ s/.kak$//;
+    open(FH, '<' . $config) or die "Unable to open\n";
+    while(<FH>) {
+	if ($_ =~ /^colorscheme/) {
+	    $config_content .= "colorscheme $theme\n";
+	    next;
+	}
+	$config_content .= $_;
+    }
+    open(FH, '>' . $config) or die "Unable to open\n";
+    print FH $config_content;
+} else {
+    print "No theme selected\n";
+}
+
